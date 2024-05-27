27977307d4cbf6e22b8029d57559644cf251f157
KatheleenAlves <katheleen.alves@hotmail.com>
2024-05-27 14:13:29 -0300

    alterações

--- exercicios/ex026(FORMS)/pegarhoras.js
+++ exercicios/ex026(FORMS)/pegarhoras.js
@@ -4,8 +4,8 @@ function carregar(){
     const dia = String(data.getDate()).padStart(2, '0') // para ter dois caracteres e caso n tenha colocar um zero
     const mes = String(data.getMonth()+ 1).padStart(2, '0')
     const ano = data.getFullYear()
-    const hora = data.getHours()
-    const minutos = data.getMinutes()
+    const hora = String(data.getHours()).padStart(2, '0')
+    const minutos = String(data.getMinutes()).padStart(2, '0')
 
     const diaProva = document.getElementById("diaProva")
     const periodoLetivo = document.getElementById("txtMes")
@@ -14,6 +14,5 @@ function carregar(){
     diaProva.value = `${ano}-${mes}-${dia}`
     periodoLetivo.value = `${ano}-${mes}`
     horaProva.value = `${hora}:${minutos}`
-
-
+    console.log(`${hora}:${minutos}`)
 }
